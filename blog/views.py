fc941aa8f6e5ea163084a3f7925839173a60a517
leslie <leslie@kali-leslie>
2021-08-23 09:49:24 +0100

    edited web.html

--- blog/views.py
+++ blog/views.py
@@ -2,21 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-#Dummy data
-posts = [
-    {
-        'authour': 'nkem',
-        'title' : 'Blog post',
-        'content' : 'First post content',
-        'date_posted' : 'August 17, 2021' 
-    },
-    {
-        'authour': 'john',
-        'title' : 'Blog post2',
-        'content' : 'ttFirst post content',
-        'date_posted' : 'August 67, 2021' 
-    }
-]
 
 # The home function to handle traffic from the home page of the blog
 
